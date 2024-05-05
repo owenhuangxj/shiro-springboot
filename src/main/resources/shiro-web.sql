9c9194ac24a398fd20d3c437c4fb8489ad8604df
owenhuangxj <wangluoke007@163.com>
2024-05-05 22:59:41 +0800

    shiro-web.sql权限经典5张表初始化脚本

---
+++ src/main/resources/shiro-web.sql
@@ -0,0 +1,116 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : mysql-home
+ Source Server Type    : MySQL
+ Source Server Version : 80032
+ Source Host           : localhost:3306
+ Source Schema         : shiro-web
+
+ Target Server Type    : MySQL
+ Target Server Version : 80032
+ File Encoding         : 65001
+
+ Date: 05/05/2024 22:56:42
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for role_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `role_permission`;
+CREATE TABLE `role_permission`  (
+  `rid` int(0) NOT NULL,
+  `pid` int(0) NOT NULL
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of role_permission
+-- ----------------------------
+INSERT INTO `role_permission` VALUES (1, 1);
+INSERT INTO `role_permission` VALUES (1, 2);
+INSERT INTO `role_permission` VALUES (1, 3);
+INSERT INTO `role_permission` VALUES (1, 4);
+INSERT INTO `role_permission` VALUES (2, 4);
+INSERT INTO `role_permission` VALUES (3, 1);
+INSERT INTO `role_permission` VALUES (3, 2);
+INSERT INTO `role_permission` VALUES (3, 4);
+INSERT INTO `role_permission` VALUES (2, 1);
+INSERT INTO `role_permission` VALUES (2, 2);
+
+-- ----------------------------
+-- Table structure for t_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `t_permission`;
+CREATE TABLE `t_permission`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `display_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_permission
+-- ----------------------------
+INSERT INTO `t_permission` VALUES (1, 'user:add', 'user:add');
+INSERT INTO `t_permission` VALUES (2, 'user:update', 'user:update');
+INSERT INTO `t_permission` VALUES (3, 'user:delete', 'user:delete');
+INSERT INTO `t_permission` VALUES (4, 'user:select', 'user:select');
+
+-- ----------------------------
+-- Table structure for t_role
+-- ----------------------------
+DROP TABLE IF EXISTS `t_role`;
+CREATE TABLE `t_role`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `display_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_role
+-- ----------------------------
+INSERT INTO `t_role` VALUES (1, 'Administer', '超级管理员');
+INSERT INTO `t_role` VALUES (2, 'Operation Manager', '运营');
+INSERT INTO `t_role` VALUES (3, 'Marketing Manager', '市场');
+
+-- ----------------------------
+-- Table structure for t_user
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user`;
+CREATE TABLE `t_user`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `salt` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_user
+-- ----------------------------
+INSERT INTO `t_user` VALUES (1, 'admin', 'admin', 'admin');
+INSERT INTO `t_user` VALUES (2, 'root', 'root', 'root');
+
+-- ----------------------------
+-- Table structure for user_role
+-- ----------------------------
+DROP TABLE IF EXISTS `user_role`;
+CREATE TABLE `user_role`  (
+  `uid` int(0) NOT NULL,
+  `rid` int(0) NOT NULL
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user_role
+-- ----------------------------
+INSERT INTO `user_role` VALUES (1, 2);
+INSERT INTO `user_role` VALUES (1, 1);
+INSERT INTO `user_role` VALUES (1, 3);
+INSERT INTO `user_role` VALUES (2, 2);
+INSERT INTO `user_role` VALUES (3, 3);
+
+SET FOREIGN_KEY_CHECKS = 1;
